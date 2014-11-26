ac08fd5cfaf56303cf5eccdd14bd4b5067dba9c2
Thomas Kliszowski <thomas@stupeflix.com>
2014-11-26 18:09:23 +0100

    Disable sorting by default, add a settings file to enable it (see Settings > JSON Reindent > Settings - Default) only on ST3.

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
-json_reindent
-=============
+JSONReindent
+============
 
-Sublime Text 2 Plugin - JSON Reindent: reindent file or selection
+Sublime Text 2/3 Plugin - JSON Reindent: reindent file or selection
 
 
 Logs
 ----
 
+- 1.1.0: Disable sorting by default, add a settings file to enable it (see Settings > JSON Reindent > Settings - Default) only on ST3.
 - 1.0.1: JSON Reindent now uses  users's tab_size instead of 2 spaces to indent json
 - 1.0: Enable user to reindent current file or selection (single or multiple), silent if it can't
--- json_reindent.py
+++ json_reindent.py
@@ -1,13 +1,21 @@
 from sublime_plugin import TextCommand
-from sublime import Region
+import sublime
+import collections
 import json
+import logging
 
-# -----------------------------------------------------------------------------
+logger = logging.getLogger(__name__)
+
+# Use settings only on ST3
+SETTINGS_FILE = "JSONReindent.sublime-settings"
+settings = {}
+
 
 class JsonReindentCommand(TextCommand):
+
     def run(self, edit, syntax_sensitive=False):
         if self.region_set_empty(self.view.sel()):
-            self.parse_region(edit, Region(0, self.view.size()))
+            self.parse_region(edit, sublime.Region(0, self.view.size()))
         else:
             region_set = self.view.sel()
             for region in region_set:
@@ -21,9 +29,32 @@ class JsonReindentCommand(TextCommand):
 
     def parse_region(self, edit, region):
         content = self.view.substr(region)
+
+        # Use OrderedDict if exists
+        params = {}
+        if hasattr(collections, 'OrderedDict'):
+            params['object_pairs_hook'] = collections.OrderedDict
+
         try:
-            json_data = json.loads(content)
-        except Exception:
-            pass
+            json_data = json.loads(
+                content,
+                strict=settings.get('strict') is True,
+                **params)
+        except Exception as e:
+            logger.error(e, exc_info=True)
         else:
-            self.view.replace(edit, region, json.dumps(json_data, indent=self.view.settings().get("tab_size", 2), sort_keys=True, separators=(',', ': ')))
+            self.view.replace(
+                edit,
+                region,
+                json.dumps(
+                    json_data,
+                    indent=self.view.settings().get("tab_size", 2),
+                    separators=(',', ': '),
+                    sort_keys=settings.get('sort_keys') is True
+                )
+            )
+
+
+def plugin_loaded():
+    global settings
+    settings = sublime.load_settings(SETTINGS_FILE)
